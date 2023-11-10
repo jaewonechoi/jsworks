8a33c51337011598bb2fee175fef3f175c04e6f0
jaewonechoi <mybity2@gmail.com>
2023-11-10 09:48:42 +0900

    for문

---
+++ day2/js/forfor.js
@@ -0,0 +1,50 @@
+// forfor.js
+//중첩 for문
+
+//5행 5열에 문자 출력
+for(let i = 0; i <= 5; i++){
+  for(let j = 1; j <=5; j++){
+    document.write("$");
+  }
+  document.write("<br>");
+}
+
+//구구단
+/*for(let i = 2; i <= 9; i++){
+  for(let j = 1; j <=9; j++){
+    document.write(i + " x " + j + " = " + (i * j) + "<br>");
+  }
+  document.write("<br>");
+}*/
+
+//직각 삼각형 모양으로 출력
+/*for(let i = 1; i <= 5; i++){
+  for(let j = 1; j <= i; j++){
+    document.write("$");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+
+for(let i = 1; i <= 5; i++){
+  for(let j = 1; j <= 6-i; j++){
+    document.write("$");
+  }
+  document.write("<br>");
+}
+
+//j--로 감소시켜서 출력하기
+for(let i = 1; i <= 5; i++){
+  for(let j = 5; j >= i; j--){
+    document.write("$");
+  }
+  document.write("<br>");
+}*/
+
+
+for(let i = 0; i < 4; i++){
+  for(let j = 1; j <= 5; j++){
+    document.write(5*i + j);
+  }
+  document.write("<br>");
+}
