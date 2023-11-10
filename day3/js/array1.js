7e19671abb692dc7de93f966ae56a6d9077d0180
jaewonechoi <mybity2@gmail.com>
2023-11-10 14:11:52 +0900

    함수, 배열

---
+++ day3/js/array1.js
@@ -0,0 +1,28 @@
+// array1.html
+//배열 사용
+//순서가 있으며 0번부터 시작함
+let animal = [];  //비어있는 배열 생성
+console.log(animal);
+console.log(animal.length + "개");
+
+//배열에 입력(저장)
+animal[0] = "cow";
+animal[1] = "pig";
+animal[2] = "horse";
+animal[3] = "cat";
+console.log(animal);
+
+//특정 요소(값)를 조회(검색)
+console.log(animal[1]);
+
+//수정(변경)
+animal[2] = "dog";
+
+//삭제
+delete animal[1];  //delete로 삭제시 공간이 남아있음
+
+//전체 요소(값) 조회(목록)
+//0번 인덱스부터 시작하고 배열의 길이 미만일 것
+for(let i = 0; i < animal.length; i++){
+  document.write(animal[i] + " ");
+}
